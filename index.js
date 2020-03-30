3ef6690f9e2381353f8e78b81a3a1c56bfe3d0c0
patient-computer-3602 <thelionking898@gmail.com>
2020-03-30 16:43:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var newObj = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
-  return newObj;
+  return object;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
