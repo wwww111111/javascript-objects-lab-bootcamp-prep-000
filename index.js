9f197c8c8e9dab3748fa3926def860566c1f96c7
patient-computer-3602 <thelionking898@gmail.com>
2020-03-30 16:36:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-newObj = Object.assign({}, object)
+newObj = Object.assign({}, object);
 
 function updateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
-  return object 
+  return object;
 }
