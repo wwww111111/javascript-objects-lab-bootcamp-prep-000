2b56515d6cb98fcf27599e0fa07cd64bb39eaefb
patient-computer-3602 <thelionking898@gmail.com>
2020-03-30 16:38:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,4 +10,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 
-deleteFromObjectByKey(object, key)
+function deleteFromObjectByKey(object, key) {
+  
+}
